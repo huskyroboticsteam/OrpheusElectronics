8b3a653ae4ee3822af622772fc79bb845c09bcf7
aavula <akhilavula2@gmail.com>
2018-10-29 19:58:03 -0700

    worked on main

--- Projects/Atmel_MC/Atmel_MC/src/main.c
+++ Projects/Atmel_MC/Atmel_MC/src/main.c
@@ -28,10 +28,21 @@
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
 #include <asf.h>
+#include <top.h>
+
+#define NUM_TASKS 5
+
+TCB commsTCB, emergencyStopTCB, encoderTCB, limit_switchTCB, pwmControlTCB;
+TCB *head, *tail;
+TCB *TCBArray[] = {&commsTCB, &emergencyStopTCB, &encoderTCB, &limit_switchTCB, &pwmControlTCB, NULL}
 
 int main (void)
 {
 	system_init();
-
 	/* Insert application code here, after the board has been initialized. */
+	initialize();
+	while(1) {
+		scheduler();
+	}
+	
 }
