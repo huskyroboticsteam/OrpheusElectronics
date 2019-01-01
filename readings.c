aacdc50c68062d0101d45306a1c34ba9df7a7650
ThrottIe <sid.kamath117@gmail.com>
2018-12-31 17:55:06 -0800

    Add files via upload

---
+++ readings.c
@@ -0,0 +1,31 @@
+
+#include <util/delay.h>
+#include <util/twi.h>
+#include <avr/include/stdio.h>
+#include "Readings.h"
+
+double moduleReadings () {
+	I2CStart(1001110); // address of slave device
+	
+	I2CWrite(0x02h); // Bus Voltage register
+	double voltage = (double) I2CRead(); // I HAVE TO FIX THIS.
+	printf("Voltage :"+ voltage);
+	if(voltage > 24 || voltage < 16){
+		soundBuzzer(0x07h); 
+	}
+	
+	I2CWrite(0x04h); // Current register
+	double current = (double) I2CRead();
+	printf("Current :"+ current);
+	
+	I2CWrite(0x03h); // Power register 
+	double power = (double) I2CRead();
+	printf("Power :"+ power);
+	
+	I2CStop();
+}
+
+void soundBuzzer (uint8_t address) { // UNFINISHED
+	I2CStart(address);
+	I2CStop();
+}
